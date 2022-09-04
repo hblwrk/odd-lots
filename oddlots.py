aa74463fae8aa139584f784cc0e0a47c5b6b17a7
ItsDysis <104866563+itsdysis@users.noreply.github.com>
2022-09-04 15:10:08 +0200

    Reformat file to be PEP compliant

--- oddlots.py
+++ oddlots.py
@@ -3,15 +3,17 @@
 import os
 import shutil
 from datetime import date, timedelta
+
 import secedgar.cik_lookup
+from discord_webhook import DiscordWebhook
 from secedgar import filings
 from secedgar.exceptions import EDGARQueryError
-from discord_webhook import DiscordWebhook
 
 # Change these accordingly
 SEC_USER_AGENT = os.environ.get("SEC_USER_AGENT", "Your Name (me@example.com)")
 TMP_FOLDER_NAME = os.environ.get("TMP_FOLDER_NAME", "tmp")
 DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL", "REPLACE WITH DISCORD WEBHOOK URL")
+
 
 def get_all_filings(date_to_download: date):
     """
@@ -31,6 +33,7 @@ def get_all_filings(date_to_download: date):
     except EDGARQueryError:
         print("Keine Filings für den gegebenen Tag!")
 
+
 def get_ticker(cik: str) -> str:
     """
     Returns the ticker to the given CIK
@@ -39,6 +42,7 @@ def get_ticker(cik: str) -> str:
     """
     result = [k for k, v in secedgar.cik_lookup.get_cik_map().get("ticker").items() if v == cik]
     return result[0] if (len(result)) > 0 else "einen unbekannten Ticker"
+
 
 def find_odd_lots():
     """
@@ -53,6 +57,7 @@ def find_odd_lots():
                     send_message_to_discord(os.path.basename(root))
     shutil.rmtree(TMP_FOLDER_NAME)
 
+
 def send_message_to_discord(cik: str):
     """
     Sends a message to the given Discord channel
@@ -64,6 +69,7 @@ def send_message_to_discord(cik: str):
         content=f"Eine neue Odd Lots Arbritage wurde für **{ticker}** gefunden. "
                 f"Hier gehts zu den Filings: https://www.sec.gov/edgar/search/#/entityName={int(cik):010}")
     webhook.execute()
+
 
 if __name__ == "__main__":
     get_all_filings(date.today() - timedelta(1))
